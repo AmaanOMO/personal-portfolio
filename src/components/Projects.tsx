8219d7ad06a6baa4d1e6acbdbe94c92104f6942c
Amaan Sheikh <Amaanomo@gmail.com>
2025-06-27 16:56:23 -0400

    okay now fully done

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { GithubIcon, ExternalLinkIcon, HomeIcon } from 'lucide-react'
+import { GithubIcon, ExternalLinkIcon } from 'lucide-react'
 import Image from 'next/image'
 
 export function Projects() {
